72773df027f04e14eb38148739c1a2106a1b897e
VasiliyTemniy <flash_er@mail.ru>
2025-03-08 14:34:03 +0300

    add basic utils

---
+++ src/utils/date.ts
@@ -0,0 +1,15 @@
+/**
+ * format date as DD.MM.YYYY__HH:mm:ss.SSS
+ * @param {number | string | Date} [date] if not provided, current datetime will be used
+ */
+export const getTimestamp = (date?: number | string | Date) => {
+  const ts = new Date(date ?? Date.now());
+  const dd = `${ts.getDate()}`.padStart(2, '0');
+  const mm = `${ts.getMonth() + 1}`.padStart(2, '0');
+  const yyyy = `${ts.getFullYear()}`.padStart(4, '0');
+  const hours = `${ts.getHours()}`.padStart(2, '0');
+  const minutes = `${ts.getMinutes()}`.padStart(2, '0');
+  const seconds = `${ts.getSeconds()}`.padStart(2, '0');
+  const milliseconds = `${ts.getMilliseconds()}`.padEnd(3, '0');
+  return `${dd}.${mm}.${yyyy}__${hours}:${minutes}:${seconds}.${milliseconds}`;
+};
---
+++ src/utils/logger.ts
@@ -0,0 +1,85 @@
+import { config } from '../config';
+import { getTimestamp } from './date';
+
+const { IS_TEST, DEBUG } = config;
+
+type TLoggable =
+  string |
+  number |
+  boolean |
+  object |
+  null |
+  undefined |
+  TLoggable[] |
+  { [key: string]: TLoggable };
+
+export interface ILogger {
+  info(...args: TLoggable[]): void;
+  warn(...args: TLoggable[]): void;
+  error(...args: TLoggable[]): void;
+  debug(...args: TLoggable[]): void;
+}
+
+export class CLogger implements ILogger {
+  constructor(protected scope: string) {
+    this.scope = scope ?? 'global';
+  }
+  
+  info(operation: string, ...args: TLoggable[]) {
+    if ( IS_TEST ) return;
+    this.print('INFO ', operation, ...args);
+  }
+  
+  warn(operation: string, ...args: TLoggable[]) {
+    if ( IS_TEST ) return;
+    this.print('WARN ', operation, ...args);
+  }
+  
+  error(operation: string, ...args: TLoggable[]) {
+    if ( IS_TEST ) return;
+    this.print('ERROR', operation, ...args);
+  }
+  
+  debug(operation: string, ...args: TLoggable[]) {
+    if ( !DEBUG.COMMON ) return;
+    this.print('DEBUG', operation, ...args);
+  }
+  
+  protected print(level: 'INFO ' | 'WARN ' | 'ERROR' | 'DEBUG', operation: string, ...args: TLoggable[]) {
+    const text = JSON.stringify([...args], null, '  ');
+    console.log(level, getTimestamp(), this.scope, operation);
+    if (text.length > 8) {
+      console.log(text);
+    } else {
+      console.log('[\n  ', ...args, '\n]');
+    }
+  }
+}
+
+export class CLoggerColored extends CLogger {
+  private colors = {
+    background: {
+      'INFO ': '\x1b[44m',
+      'WARN ': '\x1b[43m',
+      'ERROR': '\x1b[41m',
+      'DEBUG': '\x1b[100m',
+    },
+    foreground: {
+      'INFO ': '\x1b[34m',
+      'WARN ': '\x1b[33m',
+      'ERROR': '\x1b[31m',
+      'DEBUG': '\x1b[90m',
+    },
+    noColor: '\x1b[0m'
+  }
+
+  protected print(level: 'INFO ' | 'WARN ' | 'ERROR' | 'DEBUG', operation: string, ...args: TLoggable[]) {
+    const text = JSON.stringify([...args], null, '  ');
+    console.log(this.colors.background[level], level, this.colors.noColor, this.colors.foreground[level], getTimestamp(), super.scope, operation, this.colors.noColor);
+    if (text.length > 8) {
+      console.log(text);
+    } else {
+      console.log('[\n  ', ...args, '\n]');
+    }
+  }
+}
